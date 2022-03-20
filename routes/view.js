9ef934a63a74eb58d020eea05557e18d1e7c3b06
LunaLou1 <llou1@sheffield.ac.uk>
2022-03-20 03:24:27 +0000

    [room page] create room page

--- routes/view.js
+++ routes/view.js
@@ -1,16 +1,22 @@
-const express = require('express');
+/** @format */
+
+const express = require("express");
 const router = express.Router();
 
-router.get('/', function (req, res, next) {
-    res.render('index', {path: "Home"});
+router.get("/", function (req, res, next) {
+  res.render("index", { path: "Home" });
 });
 
-router.get('/about', function (req, res, next) {
-    res.render('about', {path: 'About'})
+router.get("/about", function (req, res, next) {
+  res.render("about", { path: "About" });
 });
 
-router.get('/story', (req, res) => {
-    res.render('story', {path: "Story"});
+router.get("/story", (req, res) => {
+  res.render("story", { path: "Story" });
+});
+
+router.get("/room/:roomId", (req, res) => {
+  res.render("room", { roomId: req.params["roomId"] });
 });
 
 module.exports = router;
