fd9396f46a65b319d307a27f7e3f9289e9360a57
LunaLou1 <llou1@sheffield.ac.uk>
2022-03-31 18:42:37 +0100

    [story details page]init story details page

--- public/js/public/constant.js
+++ public/js/public/constant.js
@@ -4,11 +4,10 @@
  * @desc
  * @create 11/Mar/2022 16:17
  */
-export const jq =
-  "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js";
+export const jq = "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js";
 
 export const router = [
-  { path: "/", name: "Home" },
-  { path: "/story", name: "Story" },
-  { path: "/about", name: "About" },
+    {path: "/", name: "Home"},
+    {path: "/story", name: "Story"},
+    {path: "/about", name: "About"},
 ];
