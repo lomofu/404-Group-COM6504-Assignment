550c8a3a70ace353f68cde2589e18c1c8cef97c7
LunaLou1 <llou1@sheffield.ac.uk>
2022-04-23 22:33:21 +0100

    [room] use api create new room and implement join room

--- public/js/view/storyDetail.js
+++ public/js/view/storyDetail.js
@@ -5,7 +5,11 @@
  * @date 2022/3/31
  */
 
-export const initView = (storyData, roomData) => {
+import {story, room} from "/js/public/api.js";
+
+const storyId = new URLSearchParams(window.location.search).get('storyId');
+
+export const initView = () => {
     const navHeight = $("#nav-bar").outerHeight();
 
     $("#empty-container").css("height", `${navHeight + 20}px`);
@@ -21,26 +25,42 @@ export const initView = (storyData, roomData) => {
         }
     });
 
-    _initStoryDetails(storyData);
-    _initRoomList(roomData);
+    _initStoryDetails();
+    _initRoomList();
+    _addListener();
 };
 
-const _initStoryDetails = (storyData) => {
-    const {data} = storyData;
+const _initStoryDetails = async () => {
+    const {data} = await story.getStoryDetail(storyId);
     $("#story-detail-title").text(data.title);
     $("#story-detail-author").text("@" + data.author);
     $("#story-detail-desc").text(data.description);
     $("#story-detail-img").attr("src", data.image);
 }
 
-const _initRoomList = (roomData) => {
-    const {data} = roomData;
-    data.forEach(({name, description, members}) => {
-        const room = `
+const _initRoomList = async () => {
+    $('#room-list-container').empty();
+    const {data} = await room.getRoomList(storyId);
+    data.forEach(({id, name, description, members}) => {
+        let room;
+        if (description === undefined) {
+            room = `
         <div class="w-100 card mt-3">
             <div class="card-body d-flex flex-row">
                 <div class="fs-4 me-auto">${name}</div>
-                <button class="btn btn-purple">Join</button>
+                <button id="${id}" onclick="{window.open('/room/'+this.id)}" class="btn btn-purple join-btn">Join</button>
+            </div>
+            <div class="card-footer">
+                <div class="text-start">Members: ${members}</div>
+            </div>
+        </div>
+        `;
+        } else {
+            room = `
+        <div class="w-100 card mt-3">
+            <div class="card-body d-flex flex-row">
+                <div class="fs-4 me-auto">${name}</div>
+                <button  id="${id}" onclick="{window.open('/room/'+this.id)}" class="btn btn-purple join-btn">Join</button>
             </div>
             <div class="card-footer">
                 <div class="text-start">Members: ${members}</div>
@@ -48,14 +68,61 @@ const _initRoomList = (roomData) => {
             </div>
         </div>
         `;
+        }
+
         $('#room-list-container').prepend(room);
     })
 }
 
-export const createRoom = () => {
+const _addListener = () => {
+
     const createRoomModal = document.getElementById("create-room-modal");
-    createRoomModal.addEventListener("show.bs.modal", () => {
-        console.log("!!!!!!");
-        $('#room-name-input').remove();
+    createRoomModal.addEventListener('show.bs.modal', () => {
+        $('#room-name-input').val("").css('color', 'black');
+        $('#room-desc-input').val("").css('color', 'black');
+        $('#room-name-limit').text("");
+        $('#room-desc-limit').text("");
     });
+    createRoomModal.addEventListener('hide.bs.modal', () => {
+        _initRoomList();
+    });
+
+    $('#room-name-input').keydown(() => {
+        $('#room-name-limit').text("(" + $('#room-name-input').val().length + "/30)");
+        if ($('#room-name-input').val().length > 30) {
+            $('#room-name-input').css('color', 'red');
+            $('#room-name-limit').css('color', 'red');
+        } else {
+            $('#room-name-input').css('color', 'black');
+            $('#room-name-limit').css('color', 'gray');
+        }
+    });
+
+    $('#room-desc-input').keydown(() => {
+        $('#room-desc-limit').text("(" + $('#room-desc-input').val().length + "/100)");
+        if ($('#room-desc-input').val().length > 100) {
+            $('#room-desc-input').css('color', 'red');
+            $('#room-desc-limit').css('color', 'red');
+        } else {
+            $('#room-desc-input').css('color', 'black');
+            $('#room-desc-limit').css('color', 'gray');
+        }
+    });
+
+    $('#create-btn').click(async () => {
+        if ($('#room-name-input').val().length === 0) {
+            $('#room-name-limit').text("Empty!").css('color', 'red');
+        } else if ($('#room-name-input').val().length < 30 && $('#room-desc-input').val().length < 100) {
+            const name = $('#room-name-input').val();
+            let desc = $('#room-desc-input').val();
+            const {data} = await room.createRoom({storyId, name, desc});
+            bootstrap.Modal.getInstance(createRoomModal).hide();
+            window.open('/room/' + data);
+        }
+    });
+
+}
+
+export const joinRoom = (roomId) => {
+    window.open('/room/' + roomId);
 }
