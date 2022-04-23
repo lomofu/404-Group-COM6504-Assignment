e20b6ea421b03455ce547eb2706a32f32c633ef3
LunaLou1 <llou1@sheffield.ac.uk>
2022-04-23 20:07:36 +0100

    [room api] create room related api and use api to render room list

--- public/js/view/storyDetail.js
+++ public/js/view/storyDetail.js
@@ -5,24 +5,57 @@
  * @date 2022/3/31
  */
 
-export const initView = (data) => {
-  const navHeight = $("#nav-bar").outerHeight();
+export const initView = (storyData, roomData) => {
+    const navHeight = $("#nav-bar").outerHeight();
 
-  $("#empty-container").css("height", `${navHeight + 20}px`);
+    $("#empty-container").css("height", `${navHeight + 20}px`);
 
-  $(window).scroll(() => {
-    const top = $(window).scrollTop();
-    const height = $("#nav-bar").outerHeight();
+    $(window).scroll(() => {
+        const top = $(window).scrollTop();
+        const height = $("#nav-bar").outerHeight();
 
-    if (top > height) {
-      $("#nav-bar").addClass("shadow");
-    } else {
-      $("#nav-bar").removeClass("shadow");
-    }
-  });
+        if (top > height) {
+            $("#nav-bar").addClass("shadow");
+        } else {
+            $("#nav-bar").removeClass("shadow");
+        }
+    });
 
-  $("#story-detail-title").text(data.title);
-  $("#story-detail-author").text(data.author);
-  $("#story-detail-desc").text(data.description);
-  $("#story-detail-img").attr("src", data.image);
+    _initStoryDetails(storyData);
+    _initRoomList(roomData);
 };
+
+const _initStoryDetails = (storyData) => {
+    const {data} = storyData;
+    $("#story-detail-title").text(data.title);
+    $("#story-detail-author").text("@" + data.author);
+    $("#story-detail-desc").text(data.description);
+    $("#story-detail-img").attr("src", data.image);
+}
+
+const _initRoomList = (roomData) => {
+    const {data} = roomData;
+    data.forEach(({name, description, members}) => {
+        const room = `
+        <div class="w-100 card mt-3">
+            <div class="card-body d-flex flex-row">
+                <div class="fs-4 me-auto">${name}</div>
+                <button class="btn btn-purple">Join</button>
+            </div>
+            <div class="card-footer">
+                <div class="text-start">Members: ${members}</div>
+                <div class="text-start">${description}</div>
+            </div>
+        </div>
+        `;
+        $('#room-list-container').prepend(room);
+    })
+}
+
+export const createRoom = () => {
+    const createRoomModal = document.getElementById("create-room-modal");
+    createRoomModal.addEventListener("show.bs.modal", () => {
+        console.log("!!!!!!");
+        $('#room-name-input').remove();
+    });
+}
